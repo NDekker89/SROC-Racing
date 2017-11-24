68e8273f1f8650dea52145748fb642afd91ef926
EGusic <e.gusic88@gmail.com>
2017-11-24 23:29:38 +0100

    Create Dynamic calendar

--- pages/championship_content.php
+++ pages/championship_content.php
@@ -6,325 +6,54 @@
 					
 					<div class="calendar_wrapper clearfix" id="calendar_2017">
 						
-						<div class="race finished" id="australia">
-							<div class="race_img">
-							</div>
-							<div class="race_info">
-								<div class="race_date">
-									<p><span class="rdate">12 NOVEMBER</span></p>
-								</div>
-								<div class="race_name">
-									<h3>AUSTRALIAN GRAND PRIX</h3>
-								</div>
-								<div class="race_winner">
-									<p>Fugotaro</p>
-								</div>
-							</div>
-						</div>
+						<?php 
 
-						<div class="race" id="china">
-							<div class="race_img">
-							</div>
-							<div class="race_info">
-								<div class="race_date">
-									<p><span class="rdate">19 NOVEMBER</span></p>
-								</div>
-								<div class="race_name">
-									<h3>CHINESE GRAND PRIX</h3>
-								</div>
-								<div class="race_winner">
-									<p></p>
-								</div>
-							</div>
-						</div>
+							if( ! $xml = simplexml_load_file('../data/races.xml') ) 
+							{ 
+								 echo 'unable to load XML file'; 
+							} 
+							else 
+							{ 
+								foreach( $xml as $race ) 
+								{ 
+									$id = $race->id;
+									$name = $race->name;
+									$date = $race->date;
+									$winner = $race->winner; 
 
-						<div class="race" id="bahrain">
-							<div class="race_img">
-							</div>
-							<div class="race_info">
-								<div class="race_date">
-									<p><span class="rdate">26 NOVEMBER</span></p>
-								</div>
-								<div class="race_name">
-									<h3>BAHRAIN GRAND PRIX</h3>
-								</div>
-								<div class="race_winner">
-									<p></p>
-								</div>
-							</div>
-						</div>
+									$the_date = DateTime::createFromFormat('Y-m-d', $date);
+    								$now = new DateTime();
 
-						<div class="race" id="russia">
-							<div class="race_img">
-							</div>
-							<div class="race_info">
-								<div class="race_date">
-									<p><span class="rdate">03 DECEMBER</span></p>
-								</div>
-								<div class="race_name">
-									<h3>RUSSIAN GRAND PRIX</h3>
-								</div>
-								<div class="race_winner">
-									<p></p>
-								</div>
-							</div>
-						</div>
+    								if($the_date < $now) {
+    									$finished = "finished";
+    								} else {
+    									$finished = " ";
+    								}
 
-						<div class="race" id="spain">
-							<div class="race_img">
-							</div>
-							<div class="race_info">
-								<div class="race_date">
-									<p><span class="rdate">10 DECEMBER</span></p>
-								</div>
-								<div class="race_name">
-									<h3>SPANISH GRAND PRIX</h3>
-								</div>
-								<div class="race_winner">
-									<p></p>
-								</div>
-							</div>
-						</div>
+    								$writtenDate = $the_date->format('d M Y');
 
-						<div class="race" id="monaco">
-							<div class="race_img">
-							</div>
-							<div class="race_info">
-								<div class="race_date">
-									<p><span class="rdate">17 DECEMBER</span></p>
-								</div>
-								<div class="race_name">
-									<h3>MONACO GRAND PRIX</h3>
-								</div>
-								<div class="race_winner">
-									<p></p>
-								</div>
-							</div>
-						</div>
+									?>
 
-						<div class="race" id="canada">
-							<div class="race_img">
-							</div>
-							<div class="race_info">
-								<div class="race_date">
-									<p><span class="rdate">07 JANUARY</span></p>
-								</div>
-								<div class="race_name">
-									<h3>CANADIAN GRAND PRIX</h3>
-								</div>
-								<div class="race_winner">
-									<p></p>
-								</div>
-							</div>
-						</div>
+									<div class="race <?=$finished?>" id="<?=$id?>">
+										<div class="race_img">
+										</div>
+										<div class="race_info">
+											<div class="race_date">
+												<p><span class="rdate"><?=$writtenDate?></span></p>
+											</div>
+											<div class="race_name">
+												<h3><?=$name?></h3>
+											</div>
+											<div class="race_winner">
+												<p><?=$winner?></p>
+											</div>
+										</div>
+									</div>
+									<?php
 
-						<div class="race" id="azerbaijan">
-							<div class="race_img">
-							</div>
-							<div class="race_info">
-								<div class="race_date">
-									<p><span class="rdate">14 JANUARY</span></p>
-								</div>
-								<div class="race_name">
-									<h3>AZERBAIJAN GRAND PRIX</h3>
-								</div>
-								<div class="race_winner">
-									<p></p>
-								</div>
-							</div>
-						</div>
-
-						<div class="race" id="austria">
-							<div class="race_img">
-							</div>
-							<div class="race_info">
-								<div class="race_date">
-									<p><span class="rdate">21 JANUARY</span></p>
-								</div>
-								<div class="race_name">
-									<h3>AUSTRIAN GRAND PRIX</h3>
-								</div>
-								<div class="race_winner">
-									<p></p>
-								</div>
-							</div>
-						</div>
-
-						<div class="race" id="england">
-							<div class="race_img">
-							</div>
-							<div class="race_info">
-								<div class="race_date">
-									<p><span class="rdate">28 JANUARY</span></p>
-								</div>
-								<div class="race_name">
-									<h3>BRITISH GRAND PRIX</h3>
-								</div>
-								<div class="race_winner">
-									<p></p>
-								</div>
-							</div>
-						</div>
-
-						<div class="race" id="hungary">
-							<div class="race_img">
-							</div>
-							<div class="race_info">
-								<div class="race_date">
-									<p><span class="rdate">04 FEBRUARY</span></p>
-								</div>
-								<div class="race_name">
-									<h3>HUNGARIAN GRAND PRIX</h3>
-								</div>
-								<div class="race_winner">
-									<p></p>
-								</div>
-							</div>
-						</div>
-
-						<div class="race" id="belgium">
-							<div class="race_img">
-							</div>
-							<div class="race_info">
-								<div class="race_date">
-									<p><span class="rdate">18 FEBRUARY</span></p>
-								</div>
-								<div class="race_name">
-									<h3>BELGIUM GRAND PRIX</h3>
-								</div>
-								<div class="race_winner">
-									<p></p>
-								</div>
-							</div>
-						</div>
-
-						<div class="race" id="italy">
-							<div class="race_img">
-							</div>
-							<div class="race_info">
-								<div class="race_date">
-									<p><span class="rdate">25 FEBRUARY</span></p>
-								</div>
-								<div class="race_name">
-									<h3>ITALIAN GRAND PRIX</h3>
-								</div>
-								<div class="race_winner">
-									<p></p>
-								</div>
-							</div>
-						</div>
-
-						<div class="race" id="singapore">
-							<div class="race_img">
-							</div>
-							<div class="race_info">
-								<div class="race_date">
-									<p><span class="rdate">04 MARCH</span></p>
-								</div>
-								<div class="race_name">
-									<h3>SINGAPORE GRAND PRIX</h3>
-								</div>
-								<div class="race_winner">
-									<p></p>
-								</div>
-							</div>
-						</div>
-
-						<div class="race" id="malaysia">
-							<div class="race_img">
-							</div>
-							<div class="race_info">
-								<div class="race_date">
-									<p><span class="rdate">11 MARCH</span></p>
-								</div>
-								<div class="race_name">
-									<h3>MALAYSIAN GRAND PRIX</h3>
-								</div>
-								<div class="race_winner">
-									<p></p>
-								</div>
-							</div>
-						</div>
-
-						<div class="race" id="japan">
-							<div class="race_img">
-							</div>
-							<div class="race_info">
-								<div class="race_date">
-									<p><span class="rdate">18 MARCH</span></p>
-								</div>
-								<div class="race_name">
-									<h3>JAPANESE GRAND PRIX</h3>
-								</div>
-								<div class="race_winner">
-									<p></p>
-								</div>
-							</div>
-						</div>
-						
-						<div class="race" id="us">
-							<div class="race_img">
-							</div>
-							<div class="race_info">
-								<div class="race_date">
-									<p><span class="rdate">25 MARCH</span></p>
-								</div>
-								<div class="race_name">
-									<h3>UNITED STATES GRAND PRIX</h3>
-								</div>
-								<div class="race_winner">
-									<p></p>
-								</div>
-							</div>
-						</div>
-
-						<div class="race" id="mexico">
-							<div class="race_img">
-							</div>
-							<div class="race_info">
-								<div class="race_date">
-									<p><span class="rdate">08 APRIL</span></p>
-								</div>
-								<div class="race_name">
-									<h3>MEXICAN GRAND PRIX</h3>
-								</div>
-								<div class="race_winner">
-									<p></p>
-								</div>
-							</div>
-						</div>
-
-						<div class="race" id="brasil">
-							<div class="race_img">
-							</div>
-							<div class="race_info">
-								<div class="race_date">
-									<p><span class="rdate">15 APRIL</span></p>
-								</div>
-								<div class="race_name">
-									<h3>BRASIL GRAND PRIX</h3>
-								</div>
-								<div class="race_winner">
-									<p></p>
-								</div>
-							</div>
-						</div>
-
-						<div class="race" id="abu_dhabi">
-							<div class="race_img">
-							</div>
-							<div class="race_info">
-								<div class="race_date">
-									<p><span class="rdate">22 APRIL</span></p>
-								</div>
-								<div class="race_name">
-									<h3>ABU DHABI GRAND PRIX</h3>
-								</div>
-								<div class="race_winner">
-									<p></p>
-								</div>
-							</div>
-						</div>
+								}
+							}
+						?>
 
 					</div>
 				</div>
