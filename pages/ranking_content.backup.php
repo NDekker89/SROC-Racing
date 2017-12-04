8eff641bb8a0ebdd9d5047c938422d09ebfcc100
EGusic <e.gusic88@gmail.com>
2017-12-04 20:04:14 +0100

    Dynamisch maken van de ranking+teams pagina

---
+++ pages/ranking_content.backup.php
@@ -0,0 +1,246 @@
+			<div class="container ccontent">
+				<div class="content_wrapper ranking">
+					
+					<div class="header ranking">
+						<h1>Season 4 Ranking</h1>
+					</div>
+
+					<div class="ranking_wrapper">
+
+						<table id="teams">
+							<tr>
+								<th>POS</th>
+								<th>DRIVER</th>
+								<th>TOTAL PTS</th>
+								<th>BEHIND LEAD</th>
+								<th>BEHIND NEXT</th>
+								<th>STARTS</th>
+								<th>WINS</th>
+								<th>POLES</th>
+								<th id="team_th">TEAM</th>
+							</tr>
+							<tr id="fugotaro">
+								<td class="pos"><span>1</span></td>
+								<td class="name">fugotaro</td>
+								<td class="points">43</td>
+								<td class="beh_lead">-</td>
+								<td class="beh_next">-</td>
+								<td class="starts">2</td>
+								<td class="wins">1</td>
+								<td class="poles">-</td>
+								<td class="team force_india"></td>
+							</tr>
+                            <tr id="forced">
+								<td class="pos"><span>2</span></td>
+								<td class="name">Forced</td>
+								<td class="points">25</td>
+								<td class="beh_lead">18</td>
+								<td class="beh_next">18</td>
+								<td class="starts">2</td>
+								<td class="wins">1</td>
+								<td class="poles">-</td>
+								<td class="team mclaren"></td>
+							</tr>
+                            <tr id="maydayforce">
+								<td class="pos"><span>3</span></td>
+								<td class="name">Maydayforce</td>
+								<td class="points">25</td>
+								<td class="beh_lead">18</td>
+								<td class="beh_next">18</td>
+								<td class="starts">2</td>
+								<td class="wins">-</td>
+								<td class="poles">-</td>
+								<td class="team mclaren"></td>
+							</tr>
+							<tr id="sjors538">
+								<td class="pos"><span>4</span></td>
+								<td class="name">Sjors538</td>
+								<td class="points">24</td>
+								<td class="beh_lead">19</td>
+								<td class="beh_next">1</td>
+								<td class="starts">2</td>
+								<td class="wins">-</td>
+								<td class="poles">-</td>
+								<td class="team redbull"></td>
+							</tr>
+                            <tr id="niki">
+								<td class="pos"><span>5</span></td>
+								<td class="name">Niki</td>
+								<td class="points">20</td>
+								<td class="beh_lead">23</td>
+								<td class="beh_next">1</td>
+								<td class="starts">2</td>
+								<td class="wins">-</td>
+								<td class="poles">1</td>
+								<td class="team ferari"></td>
+							</tr>
+								<tr id="przewalskipaard">
+								<td class="pos"><span>6</span></td>
+								<td class="name">Przewalskipaard</td>
+								<td class="points">19</td>
+								<td class="beh_lead">24</td>
+								<td class="beh_next">1</td>
+								<td class="starts">2</td>
+								<td class="wins">-</td>
+								<td class="poles">-</td>
+								<td class="team ferari"></td>
+							</tr>
+                            <tr id="brix">
+								<td class="pos"><span>7</span></td>
+								<td class="name">Brix</td>
+								<td class="points">12</td>
+								<td class="beh_lead">31</td>
+								<td class="beh_next">7</td>
+								<td class="starts">1</td>
+								<td class="wins">-</td>
+								<td class="poles">-</td>
+								<td class="team williams"></td>
+							</tr>
+                            <tr id="ananas">
+								<td class="pos"><span>8</span></td>
+								<td class="name">Ananas</td>
+								<td class="points">10</td>
+								<td class="beh_lead">33</td>
+								<td class="beh_next">2</td>
+								<td class="starts">2</td>
+								<td class="wins">-</td>
+								<td class="poles">1</td>
+								<td class="team renault"></td>
+							</tr>
+							<tr id="fluffy">
+								<td class="pos"><span>9</span></td>
+								<td class="name">Fluffy</td>
+								<td class="points">8</td>
+								<td class="beh_lead">35</td>
+								<td class="beh_next">2</td>
+								<td class="starts">2</td>
+								<td class="wins">-</td>
+								<td class="poles">-</td>
+								<td class="team tororosso"></td>
+							</tr>
+							<tr id="vivalatrance">
+								<td class="pos"><span>10</span></td>
+								<td class="name">VivaLaTrance</td>
+								<td class="points">8</td>
+								<td class="beh_lead">35</td>
+								<td class="beh_next">2</td>
+								<td class="starts">2</td>
+								<td class="wins">-</td>
+								<td class="poles">-</td>
+								<td class="team renault"></td>
+							</tr>
+							<tr id="watch_your_6">
+								<td class="pos"><span>11</span></td>
+								<td class="name">WATCH YOUR 6</td>
+								<td class="points">5</td>
+								<td class="beh_lead">38</td>
+								<td class="beh_next">3</td>
+								<td class="starts">2</td>
+								<td class="wins">-</td>
+								<td class="poles">-</td>
+								<td class="team redbull"></td>
+							</tr>
+							<tr id="isolaaa">
+								<td class="pos"><span>12</span></td>
+								<td class="name">Isolaaa</td>
+								<td class="points">2</td>
+								<td class="beh_lead">41</td>
+								<td class="beh_next">3</td>
+								<td class="starts">2</td>
+								<td class="wins">-</td>
+								<td class="poles">-</td>
+								<td class="team sauber"></td>
+							</tr>
+							<tr id="keplur">
+								<td class="pos"><span>13</span></td>
+								<td class="name">Keplur</td>
+								<td class="points">1</td>
+								<td class="beh_lead">42</td>
+								<td class="beh_next">1</td>
+								<td class="starts">1</td>
+								<td class="wins">-</td>
+								<td class="poles">-</td>
+								<td class="team sauber"></td>
+							</tr>
+							<tr id="ziemnioczek">
+								<td class="pos"><span>14</span></td>
+								<td class="name">Ziemnioczek</td>
+								<td class="points">-</td>
+								<td class="beh_lead">43</td>
+								<td class="beh_next">1</td>
+								<td class="starts">2</td>
+								<td class="wins">-</td>
+								<td class="poles">-</td>
+								<td class="team force_india"></td>
+							</tr>
+							<tr id="dolan">
+								<td class="pos"><span>15</span></td>
+								<td class="name">dolan</td>
+								<td class="points">-</td>
+								<td class="beh_lead">43</td>
+								<td class="beh_next">1</td>
+								<td class="starts">2</td>
+								<td class="wins">-</td>
+								<td class="poles">-</td>
+								<td class="team mercedes"></td>
+							</tr>
+							<tr id="samvanzantvliet">
+								<td class="pos"><span>16</span></td>
+								<td class="name">samvanzantvliet</td>
+								<td class="points">-</td>
+								<td class="beh_lead">43</td>
+								<td class="beh_next">1</td>
+								<td class="starts">2</td>
+								<td class="wins">-</td>
+								<td class="poles">-</td>
+								<td class="team mercedes"></td>
+							</tr>
+							<tr id="bassie69">
+								<td class="pos"><span>17</span></td>
+								<td class="name">Bassie-69</td>
+								<td class="points">-</td>
+								<td class="beh_lead">43</td>
+								<td class="beh_next">1</td>
+								<td class="starts">2</td>
+								<td class="wins">-</td>
+								<td class="poles">-</td>
+								<td class="team tororosso"></td>
+							</tr>
+							<tr id="chickenskin">
+								<td class="pos"><span>18</span></td>
+								<td class="name">Chickenskin</td>
+								<td class="points">-</td>
+								<td class="beh_lead">43</td>
+								<td class="beh_next">1</td>
+								<td class="starts">2</td>
+								<td class="wins">-</td>
+								<td class="poles">-</td>
+								<td class="team williams"></td>
+                            </tr>
+                            <tr id="bizy">
+								<td class="pos"><span>19</span></td>
+								<td class="name">Bizy</td>
+								<td class="points">-</td>
+								<td class="beh_lead">43</td>
+								<td class="beh_next">1</td>
+								<td class="starts">1</td>
+								<td class="wins">-</td>
+								<td class="poles">-</td>
+								<td class="team haas"></td>
+							</tr>
+                            <tr id="will_i_am">
+								<td class="pos"><span>20</span></td>
+								<td class="name">WILL I AM</td>
+								<td class="points">-</td>
+								<td class="beh_lead">43</td>
+								<td class="beh_next">1</td>
+								<td class="starts">1</td>
+								<td class="wins">-</td>
+								<td class="poles">-</td>
+								<td class="team haas"></td>
+							</tr>
+						</table>
+
+					</div>
+				</div>
+			</div>
--- pages/ranking_content.php
+++ pages/ranking_content.php
@@ -19,226 +19,81 @@
 								<th>POLES</th>
 								<th id="team_th">TEAM</th>
 							</tr>
-							<tr id="fugotaro">
-								<td class="pos"><span>1</span></td>
-								<td class="name">fugotaro</td>
-								<td class="points">43</td>
-								<td class="beh_lead">-</td>
-								<td class="beh_next">-</td>
-								<td class="starts">2</td>
-								<td class="wins">1</td>
-								<td class="poles">-</td>
-								<td class="team force_india"></td>
-							</tr>
-                                                        <tr id="forced">
-								<td class="pos"><span>2</span></td>
-								<td class="name">Forced</td>
-								<td class="points">25</td>
-								<td class="beh_lead">18</td>
-								<td class="beh_next">18</td>
-								<td class="starts">2</td>
-								<td class="wins">1</td>
-								<td class="poles">-</td>
-								<td class="team mclaren"></td>
-							</tr>
-                                                        <tr id="maydayforce">
-								<td class="pos"><span>3</span></td>
-								<td class="name">Maydayforce</td>
-								<td class="points">25</td>
-								<td class="beh_lead">18</td>
-								<td class="beh_next">18</td>
-								<td class="starts">2</td>
-								<td class="wins">-</td>
-								<td class="poles">-</td>
-								<td class="team mclaren"></td>
-							</tr>
-							<tr id="sjors538">
-								<td class="pos"><span>4</span></td>
-								<td class="name">Sjors538</td>
-								<td class="points">24</td>
-								<td class="beh_lead">19</td>
-								<td class="beh_next">1</td>
-								<td class="starts">2</td>
-								<td class="wins">-</td>
-								<td class="poles">-</td>
-								<td class="team redbull"></td>
-							</tr>
-                                                        <tr id="niki">
-								<td class="pos"><span>5</span></td>
-								<td class="name">Niki</td>
-								<td class="points">20</td>
-								<td class="beh_lead">23</td>
-								<td class="beh_next">1</td>
-								<td class="starts">2</td>
-								<td class="wins">-</td>
-								<td class="poles">1</td>
-								<td class="team ferari"></td>
-							</tr>
-								<tr id="przewalskipaard">
-								<td class="pos"><span>6</span></td>
-								<td class="name">Przewalskipaard</td>
-								<td class="points">19</td>
-								<td class="beh_lead">24</td>
-								<td class="beh_next">1</td>
-								<td class="starts">2</td>
-								<td class="wins">-</td>
-								<td class="poles">-</td>
-								<td class="team ferari"></td>
-							</tr>
-                                                        <tr id="brix">
-								<td class="pos"><span>7</span></td>
-								<td class="name">Brix</td>
-								<td class="points">12</td>
-								<td class="beh_lead">31</td>
-								<td class="beh_next">7</td>
-								<td class="starts">1</td>
-								<td class="wins">-</td>
-								<td class="poles">-</td>
-								<td class="team williams"></td>
-							</tr>
-                                                        <tr id="ananas">
-								<td class="pos"><span>8</span></td>
-								<td class="name">Ananas</td>
-								<td class="points">10</td>
-								<td class="beh_lead">33</td>
-								<td class="beh_next">2</td>
-								<td class="starts">2</td>
-								<td class="wins">-</td>
-								<td class="poles">1</td>
-								<td class="team renault"></td>
-							</tr>
-							<tr id="fluffy">
-								<td class="pos"><span>9</span></td>
-								<td class="name">Fluffy</td>
-								<td class="points">8</td>
-								<td class="beh_lead">35</td>
-								<td class="beh_next">2</td>
-								<td class="starts">2</td>
-								<td class="wins">-</td>
-								<td class="poles">-</td>
-								<td class="team tororosso"></td>
-							</tr>
-							<tr id="vivalatrance">
-								<td class="pos"><span>10</span></td>
-								<td class="name">VivaLaTrance</td>
-								<td class="points">8</td>
-								<td class="beh_lead">35</td>
-								<td class="beh_next">2</td>
-								<td class="starts">2</td>
-								<td class="wins">-</td>
-								<td class="poles">-</td>
-								<td class="team renault"></td>
-							</tr>
-							<tr id="watch_your_6">
-								<td class="pos"><span>11</span></td>
-								<td class="name">WATCH YOUR 6</td>
-								<td class="points">5</td>
-								<td class="beh_lead">38</td>
-								<td class="beh_next">3</td>
-								<td class="starts">2</td>
-								<td class="wins">-</td>
-								<td class="poles">-</td>
-								<td class="team redbull"></td>
-							</tr>
-								<tr id="isolaaa">
-								<td class="pos"><span>12</span></td>
-								<td class="name">Isolaaa</td>
-								<td class="points">2</td>
-								<td class="beh_lead">41</td>
-								<td class="beh_next">3</td>
-								<td class="starts">2</td>
-								<td class="wins">-</td>
-								<td class="poles">-</td>
-								<td class="team sauber"></td>
-							</tr>
-							<tr id="keplur">
-								<td class="pos"><span>13</span></td>
-								<td class="name">Keplur</td>
-								<td class="points">1</td>
-								<td class="beh_lead">42</td>
-								<td class="beh_next">1</td>
-								<td class="starts">1</td>
-								<td class="wins">-</td>
-								<td class="poles">-</td>
-								<td class="team sauber"></td>
-							</tr>
-							<tr id="ziemnioczek">
-								<td class="pos"><span>14</span></td>
-								<td class="name">Ziemnioczek</td>
-								<td class="points">-</td>
-								<td class="beh_lead">43</td>
-								<td class="beh_next">1</td>
-								<td class="starts">2</td>
-								<td class="wins">-</td>
-								<td class="poles">-</td>
-								<td class="team force_india"></td>
-							</tr>
-							<tr id="dolan">
-								<td class="pos"><span>15</span></td>
-								<td class="name">dolan</td>
-								<td class="points">-</td>
-								<td class="beh_lead">43</td>
-								<td class="beh_next">1</td>
-								<td class="starts">2</td>
-								<td class="wins">-</td>
-								<td class="poles">-</td>
-								<td class="team mercedes"></td>
-							</tr>
-							<tr id="samvanzantvliet">
-								<td class="pos"><span>16</span></td>
-								<td class="name">samvanzantvliet</td>
-								<td class="points">-</td>
-								<td class="beh_lead">43</td>
-								<td class="beh_next">1</td>
-								<td class="starts">2</td>
-								<td class="wins">-</td>
-								<td class="poles">-</td>
-								<td class="team mercedes"></td>
-							</tr>
-							<tr id="bassie69">
-								<td class="pos"><span>17</span></td>
-								<td class="name">Bassie-69</td>
-								<td class="points">-</td>
-								<td class="beh_lead">43</td>
-								<td class="beh_next">1</td>
-								<td class="starts">2</td>
-								<td class="wins">-</td>
-								<td class="poles">-</td>
-								<td class="team tororosso"></td>
-							</tr>
-							<tr id="chickenskin">
-								<td class="pos"><span>18</span></td>
-								<td class="name">Chickenskin</td>
-								<td class="points">-</td>
-								<td class="beh_lead">43</td>
-								<td class="beh_next">1</td>
-								<td class="starts">2</td>
-								<td class="wins">-</td>
-								<td class="poles">-</td>
-								<td class="team williams"></td>
-                                                        </tr>
-                                                        <tr id="bizy">
-								<td class="pos"><span>19</span></td>
-								<td class="name">Bizy</td>
-								<td class="points">-</td>
-								<td class="beh_lead">43</td>
-								<td class="beh_next">1</td>
-								<td class="starts">1</td>
-								<td class="wins">-</td>
-								<td class="poles">-</td>
-								<td class="team haas"></td>
-							</tr>
-                                                        <tr id="will_i_am">
-								<td class="pos"><span>20</span></td>
-								<td class="name">WILL I AM</td>
-								<td class="points">-</td>
-								<td class="beh_lead">43</td>
-								<td class="beh_next">1</td>
-								<td class="starts">1</td>
-								<td class="wins">-</td>
-								<td class="poles">-</td>
-								<td class="team haas"></td>
-							</tr>
+
+
+							<?php 
+
+								// Enable displaying of errors
+								// (!) Disable on PRD environment
+								//ini_set('display_errors', 'On');
+								//error_reporting(E_ALL | E_STRICT);
+						
+								include("../php/class_lib.php");
+
+								// Create array for the drivers
+								$driversArray = [];
+
+								// Load XML file, create driver objects and add them to array
+								if( ! $xml = simplexml_load_file('../data/drivers.xml') ) 
+								{ 
+									 echo 'unable to load XML file'; 
+								} 
+								else 
+								{ 
+									foreach ($xml as $driver) {
+										$driversArray[] = new driver(
+											$driver->id, 
+											$driver->name, 
+											$driver->totalPoints, 
+											$driver->starts,
+											$driver->wins,
+											$driver->poles,
+											$driver->team);
+									}
+								}
+								
+								// To-do: Try catch
+
+								// Sort Array
+								 usort($driversArray, "sort_drivers_points_desc");
+
+								// Get points of nr1
+								$mostPoints = $driversArray[0]->getPoints();
+
+								// Get previous index in array
+								$previous = 0;
+
+								// Output data to HTML
+								foreach ($driversArray as $key => $driver) {
+
+									// Calculate the position by looking at the index key
+									$position = $key +1;
+
+									// Get points of the above driver
+									$nextPoints = $driversArray[$previous]->getPoints();
+
+									?>
+
+									<tr id="<?=$driver->getId()?>">
+										<td class="pos"><span><?=$position?></span></td>
+										<td class="name"><?=$driver->get_name()?></td>
+										<td class="points"><?=$driver->getPoints()?></td>
+										<td class="beh_lead"><?=calcBehindLead($mostPoints, $driver->getPoints())?></td>
+										<td class="beh_next"><?=calcBehindNext($nextPoints, $driver->getPoints())?></td>
+										<td class="starts"><?=$driver->getStarts()?></td>
+										<td class="wins"><?=$driver->getWins()?></td>
+										<td class="poles"><?=$driver->getPoles()?> </td>
+										<td class="team <?=$driver->getTeam()?>"></td>
+									</tr>
+
+								 	<?php
+
+								 	// Set current driver as the "above" driver
+								 	$previous = $key;
+								}	
+									
+							?>                    
+
 						</table>
 
 					</div>
